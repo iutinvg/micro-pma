ef85c69b187406ebcca94ba9a467e3f54943de3b
Slava <slava@kathleen.site>
2011-02-03 09:20:51 +0600

    add readme

---
+++ README.markdown
@@ -0,0 +1,26 @@
+﻿micro-pma - a very small MySQL admin tool written with PHP
+----------------------------------------------------------
+
+This tool allows you to manage the database in case you have very limited access
+to your host: only WEB and FTP. Sometimes responsible persons forget to provide
+you with SSH access, sometimes they are too lazy or not competent to install
+PhpMyAdmin for you. In this case you can use this small script for some rapid
+and simple manipulations with MySQL databases.
+
+Features
+========
+
+- Keeps SQL queries history within the current session, very useful for
+  repeatable operations.
+- Provides VERY SIMPLE security protection: you can setup password to avoid
+  malicious access. But it worth removing this script when you are done.
+- Written with basic mysql-functions. It works on PHP 5 and PHP 4.
+
+Usage
+=====
+
+- Open pma.php in editor and write your database credentials: host, database
+  name, username and password.
+- Change script access password also.
+- Upload the file to web-directory.
+- Go to this script with proper URL in your browser.
---
+++ pma.php
@@ -0,0 +1,118 @@
+<?php
+
+// setup
+define('DB_HOST', 'localhost');
+define('DB_USER', 'midoilw');
+define('DB_NAME', 'MO_Products');
+define('DB_PASS', '45produ367');
+define('SCRIPT_PASS', 'passw0rd');
+
+session_start();
+
+class PMA {
+
+    // create one log row
+    function query($query) {
+        $link = PMA::__connect();
+        if ($link == null) {
+            return $link;
+        }
+
+        if (!isset($_SESSION['pma_history'])) {
+            $_SESSION['pma_history'] = array();
+        }
+
+        array_unshift($_SESSION['pma_history'], $query);
+
+        $res = @mysql_query($query);
+        if (!$res) {
+            echo "\nQuery failed: " . mysql_error();
+            echo "\nQuery: " . $query;
+        }
+
+        return $res;
+    }
+
+    function select($query) {
+        $result = PMA::query($query);
+
+        if ($result == null) {
+            return $result;
+        }
+
+        // Printing results in HTML
+        echo "<table border=1>\n";
+        $row = 0;
+        while ($line = @mysql_fetch_array($result, MYSQL_ASSOC)) {
+
+            if ($row == 0) {
+                foreach (array_keys($line) as $col_name) {
+                    echo "\t\t<th>$col_name</th>\n";
+                }
+            }
+            $color = (($row % 2) == 1) ? '#cccccc' : 'white';
+            $row++;
+            echo "\t<tr bgcolor='{$color}'>\n";
+            foreach ($line as $col_value) {
+                echo "\t\t<td>$col_value</td>\n";
+            }
+            echo "\t</tr>\n";
+        }
+        echo "</table>\n";
+
+        // Free resultset
+        @mysql_free_result($result);
+    }
+
+    function __connect() {
+        // Connecting, selecting database
+        if (!($link = @mysql_connect(DB_HOST, DB_USER, DB_PASS))) {
+            echo "\nCould not connect: " . mysql_error();
+            return null;
+        }
+        if (!@mysql_select_db(DB_NAME)) {
+            echo "\nCould not select database";
+            return null;
+        }
+
+        return $link;
+    }
+
+}
+
+echo '<html><title>Small MySQL client</title><body>';
+
+echo '<pre>';
+
+// check secure
+if (isset($_REQUEST['q']) && $_REQUEST['p'] != SCRIPT_PASS) {
+    echo "Enter a correct password, bub!";
+}
+
+$query = isset($_REQUEST['q']) ? stripslashes($_REQUEST['q']) : '';
+$pass = isset($_REQUEST['p']) ? $_REQUEST['p'] : '';
+
+if (isset($_REQUEST['q'])) {
+    PMA::select($query);
+}
+
+echo '</pre></hr>';
+
+echo "\n\n<form action='pma.php' method='post'>";
+
+echo "\n\nPassword: <input type='password' value='{$pass}' name='p'><br>";
+
+echo "\n\nQuery:<br><textarea cols='66' rows='10' name='q'>{$query}</textarea><br>";
+
+echo "\n\n<input type='submit'></form>";
+
+if (count($_SESSION['pma_history'])) {
+    echo '<h3>History:</h3><pre>';
+    foreach ($_SESSION['pma_history'] as $hist) {
+        echo "{$hist}\n";
+    }
+    echo '<pre>';
+}
+
+echo "\n\n</body></html>";
+?>
