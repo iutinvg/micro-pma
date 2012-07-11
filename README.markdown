65bfad90e06270d11a410563496f86347590f933
iutinvg <iutin@whirix.com>
2012-07-11 11:07:43 +0700

    sql dump function; fix bugs

--- README.markdown
+++ README.markdown
@@ -1,5 +1,5 @@
-﻿micro-pma - a very small MySQL admin tool written with PHP
-==========================================================
+﻿micro-pma is a very small MySQL admin tool written with PHP
+===========================================================
 
 This tool allows you to manage the database in case you have very limited access
 to your host: only WEB and FTP. Sometimes responsible persons forget to provide
@@ -12,6 +12,7 @@ Features
 
 - It keeps SQL queries history within the current session (very useful for
   repeatable operations).
+- Can be used to dump small tables/databases.
 - It provides VERY SIMPLE security protection: you can setup password to avoid
   malicious access. But it worth removing this script when you are done.
 - Written with basic mysql-functions. It works on PHP 5 and PHP 4.
@@ -21,6 +22,12 @@ Usage
 
 - Open pma.php in editor and write your database credentials: host, database
   name, username and password.
-- Also, change script access password.
+- Also, change default script access password.
 - Upload the file to web-directory.
 - Go to this script with proper URL in your browser.
+
+Thanks
+------
+
+I wasn't able to define the real author of database dump piece of code. 
+Anyway, it is for him: THANK YOU, YOU ARE [AWESOME](http://www.codinghorror.com/blog/2012/05/how-to-stop-sucking-and-be-awesome-instead.html).
--- pma.php
+++ pma.php
@@ -1,6 +1,6 @@
 <?php
-/* 
- * Copyright (c) 2011 Whirix <info@whirix.com>
+/*
+ * Copyright (c) Whirix <info@whirix.com>
  * License: http://www.opensource.org/licenses/mit-license.html
  */
 
@@ -9,113 +9,183 @@ define('DB_HOST', 'localhost');
 define('DB_NAME', 'db-name');
 define('DB_USER', 'db-user');
 define('DB_PASS', 'db-pass');
-define('SCRIPT_PASS', 'passw0rd');
+// change it! will not work with default password
+define('SCRIPT_PASS', 'change me!!!');
 
 session_start();
 
 class PMA {
 
-    // create one log row
-    function query($query) {
-        $link = PMA::__connect();
-        if ($link == null) {
-            return $link;
-        }
+	// create one log row
+	function query($query) {
+		$link = PMA::__connect();
+		if ($link == null) {
+			return $link;
+		}
 
-        if (!isset($_SESSION['pma_history'])) {
-            $_SESSION['pma_history'] = array();
-        }
+		if (!isset($_SESSION['pma_history'])) {
+			$_SESSION['pma_history'] = array();
+		}
 
-        array_unshift($_SESSION['pma_history'], $query);
+		array_unshift($_SESSION['pma_history'], $query);
 
-        $res = @mysql_query($query);
-        if (!$res) {
-            echo "\nQuery failed: " . mysql_error();
-            echo "\nQuery: " . $query;
-        }
+		$res = @mysql_query($query);
+		if (!$res) {
+			echo "\nQuery failed: " . mysql_error();
+			echo "\nQuery: " . $query;
+		}
 
-        return $res;
-    }
+		return $res;
+	}
 
-    function select($query) {
-        $result = PMA::query($query);
+	function select($query) {
+		$result = PMA::query($query);
 
-        if ($result == null) {
-            return $result;
-        }
+		if ($result == null) {
+			return $result;
+		}
 
-        // Printing results in HTML
-        echo "<table border=1>\n";
-        $row = 0;
-        while ($line = @mysql_fetch_array($result, MYSQL_ASSOC)) {
+		// Printing results in HTML
+		echo "<table border=1>\n";
+		$row = 0;
+		while ($line = @mysql_fetch_array($result, MYSQL_ASSOC)) {
 
-            if ($row == 0) {
-                foreach (array_keys($line) as $col_name) {
-                    echo "\t\t<th>$col_name</th>\n";
-                }
-            }
-            $color = (($row % 2) == 1) ? '#cccccc' : 'white';
-            $row++;
-            echo "\t<tr bgcolor='{$color}'>\n";
-            foreach ($line as $col_value) {
-                echo "\t\t<td>$col_value</td>\n";
-            }
-            echo "\t</tr>\n";
-        }
-        echo "</table>\n";
+			if ($row == 0) {
+				foreach (array_keys($line) as $col_name) {
+					echo "\t\t<th>$col_name</th>\n";
+				}
+			}
+			$color = (($row % 2) == 1) ? '#cccccc' : 'white';
+			$row++;
+			echo "\t<tr bgcolor='{$color}'>\n";
+			foreach ($line as $col_value) {
+				echo "\t\t<td>$col_value</td>\n";
+			}
+			echo "\t</tr>\n";
+		}
+		echo "</table>\n";
 
-        // Free resultset
-        @mysql_free_result($result);
-    }
+		// Free resultset
+		@mysql_free_result($result);
+	}
 
-    function __connect() {
-        // Connecting, selecting database
-        if (!($link = @mysql_connect(DB_HOST, DB_USER, DB_PASS))) {
-            echo "\nCould not connect: " . mysql_error();
-            return null;
-        }
-        if (!@mysql_select_db(DB_NAME)) {
-            echo "\nCould not select database";
-            return null;
-        }
+	function __connect() {
+		// Connecting, selecting database
+		if (!($link = @mysql_connect(DB_HOST, DB_USER, DB_PASS))) {
+			echo "\nCould not connect: " . mysql_error();
+			return null;
+		}
+		if (!@mysql_select_db(DB_NAME)) {
+			echo "\nCould not select database";
+			return null;
+		}
 
-        return $link;
-    }
+		return $link;
+	}
+
+	function backup_tables($host, $user, $pass, $name, $tables = '*') {
+
+		$link = mysql_connect($host, $user, $pass);
+		mysql_select_db($name, $link);
+
+		//get all of the tables
+		if ($tables == '*') {
+			$tables = array();
+			$result = mysql_query('SHOW TABLES');
+			while ($row = mysql_fetch_row($result)) {
+				$tables[] = $row[0];
+			}
+		} else {
+			$tables = is_array($tables) ? $tables : explode(',', $tables);
+		}
+
+		//cycle through
+		foreach ($tables as $table) {
+			$result = mysql_query('SELECT * FROM ' . $table);
+			$num_fields = mysql_num_fields($result);
+
+			//$return.= 'DROP TABLE ' . $table . ';';
+			$row2 = mysql_fetch_row(mysql_query('SHOW CREATE TABLE ' . $table));
+			$return .= "\n\n" . $row2[1] . ";\n\n";
+
+			for ($i = 0; $i < $num_fields; $i++) {
+				while ($row = mysql_fetch_row($result)) {
+					$return .= 'INSERT INTO ' . $table . ' VALUES(';
+					for ($j = 0; $j < $num_fields; $j++) {
+						$row[$j] = addslashes($row[$j]);
+						$row[$j] = ereg_replace("\n", "\\n", $row[$j]);
+						if (isset($row[$j])) {
+							$return .= '"' . $row[$j] . '"';
+						} else {
+							$return .= '""';
+						}
+						if ($j < ($num_fields - 1)) {
+							$return .= ',';
+						}
+					}
+					$return .= ");\n";
+				}
+			}
+			$return .= "\n\n\n";
+		}
+
+		//save file
+		$filename = 'dump-' . date('c') . '.sql';
+		header("Content-type: application/octet-stream");
+		header("Content-Disposition: attachment; filename={$filename}");
+		header("Pragma: no-cache");
+		header("Expires: 0");
+
+		echo $return;
+		exit();
+	}
+
+	function secure() {
+		if (SCRIPT_PASS == 'change me!!!') {
+			die("Do not use default password!");
+		}
+		if ($_REQUEST['p'] != SCRIPT_PASS) {
+			die("Enter a correct password, bub!");
+		}
+		return TRUE;
+	}
 
 }
 
-echo '<html><title>Small MySQL client</title><body>';
+echo '<html><title>Micro PHP MySQL Admin</title><body>';
 
 echo '<pre>';
 
 // check secure
-if (isset($_REQUEST['q']) && $_REQUEST['p'] != SCRIPT_PASS) {
-    echo "Enter a correct password, bub!";
-}
-
-$query = isset($_REQUEST['q']) ? stripslashes($_REQUEST['q']) : '';
-$pass = isset($_REQUEST['p']) ? $_REQUEST['p'] : '';
-
-if (isset($_REQUEST['q'])) {
-    PMA::select($query);
+if (!empty($_REQUEST['q']) && !empty($_REQUEST['submitquery'])) {
+	if (PMA::secure()) {
+		$query = stripslashes($_REQUEST['q']);
+		PMA::select($query);
+	}
+} else if (!empty($_REQUEST['tables']) && !empty($_REQUEST['submitdump'])) {
+	if (PMA::secure()) {
+		PMA::backup_tables(DB_HOST, DB_USER, DB_PASS, DB_NAME, $_REQUEST['tables']);
+	}
 }
 
 echo '</pre></hr>';
 
-echo "\n\n<form action='pma.php' method='post'>";
+echo "\n\n<form action='' method='post'>";
 
-echo "\n\nPassword: <input type='password' value='{$pass}' name='p'><br>";
+echo "\n\nPassword: <input type='password' value='{$_REQUEST['p']}' name='p'><br><br>";
+
+echo "\n\n<input type='text' value='{$_REQUEST['tables']}' name='tables'><input type='submit' name='submitdump' value='Dump Table'> * for all tables<br><br>";
 
 echo "\n\nQuery:<br><textarea cols='66' rows='10' name='q'>{$query}</textarea><br>";
 
-echo "\n\n<input type='submit'></form>";
+echo "\n\n<input type='submit' name='submitquery' value='Submit Query'></form>";
 
 if (count($_SESSION['pma_history'])) {
-    echo '<h3>History:</h3><pre>';
-    foreach ($_SESSION['pma_history'] as $hist) {
-        echo "{$hist}\n";
-    }
-    echo '<pre>';
+	echo '<h3>History:</h3><pre>';
+	foreach ($_SESSION['pma_history'] as $hist) {
+		echo "{$hist}\n";
+	}
+	echo '<pre>';
 }
 
 echo "\n\n</body></html>";
